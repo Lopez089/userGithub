e56e9efb2d7475f177c6f3aa23ac5c23054f78da
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-09-05 23:19:02 +0200

    feat(C Layaut): create componet layout

---
+++ components/layout.js
@@ -0,0 +1,11 @@
+export const layout = ({ children }) => {
+  return (
+    <main>
+      <section>
+        <h1>devfinder</h1>
+        <p>butonlive</p>
+      </section>
+      {children}
+    </main>
+  )
+}
