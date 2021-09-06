9bdea3862ca67146dfcf07c91983617140865e7a
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-09-06 17:29:53 +0200

    fix (test) : delete test than not use

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import App from './App'
 
 describe('App', () => {
   test('renderiza una texto hola', () => {
     render(<App />)
-    screen.getAllByText('#patch')
   })
 })
