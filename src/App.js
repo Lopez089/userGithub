fa3fd1073a87e39c0e61b17f6ae6b525f6b15aba
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-09-06 17:21:49 +0200

    feat (app): buil app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { ContentInfoUser } from './components/ContentInfoUser'
+import { Search } from './components/search'
+import { Layout } from './components/layout'
+import { DataUser } from './components/dataUser'
+import { DetailUser } from './components/detailUser'
+import { DetailGithub } from './components/detailGithub'
+
 const App = () => {
   return (
     <div>
-      <p>#patch</p>
+      <Layout>
+        <Search />
+        <ContentInfoUser>
+          <DataUser />
+          <DetailGithub />
+          <DetailUser />
+        </ContentInfoUser>
+      </Layout>
     </div>
   )
 }
