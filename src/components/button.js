e5df8041ef15ac7f83478630c6ad2ec5cc75be86
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-09-07 20:17:06 +0200

    refactor (css) extrayendo estilos al fichero componente

--- src/components/button.js
+++ src/components/button.js
@@ -1,21 +1,4 @@
-import styled from 'styled-components'
-
-export const ButtonStyle = styled.button`
-  margin:0.5rem;
-  padding: 1em;
-  border-radius: 1rem;
-  background: ${props => props.theme.secondary};
-  color: ${props => props.theme.text_primary};
-  font-weight: 200;
-  border: 0;
-  cursor: pointer;
-  :hover  {
-    background: ${props => props.theme.primary_hover};
-  }
-  :active  {
-    background: ${props => props.theme.primary};
-  }
-`
+import { ButtonStyle } from '../styles/components'
 
 export const Button = ({ children }) => {
   return (
--- src/styles/components.js
+++ src/styles/components.js
@@ -47,3 +47,19 @@ export const Icon = styled.div`
   margin-left:1rem;
   color:${props => props.theme.secondary};
 `
+export const ButtonStyle = styled.button`
+  margin:0.5rem;
+  padding: 1em;
+  border-radius: 1rem;
+  background: ${props => props.theme.secondary};
+  color: ${props => props.theme.text_primary};
+  font-weight: 200;
+  border: 0;
+  cursor: pointer;
+  :hover  {
+    background: ${props => props.theme.primary_hover};
+  }
+  :active  {
+    background: ${props => props.theme.primary};
+  }
+`
