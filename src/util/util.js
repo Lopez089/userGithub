765bba61e3918ea19b2c5e430ab7fc967bc891e3
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-09-07 10:17:35 +0200

    refactor (util): extract util getUser to file end

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,15 @@
+import axios from 'axios'
+import env from 'react-dotenv'
+import { uri, userDefault } from '../public/index'
+
 export const setInputChange = (e, setInputValue) => {
   setInputValue(e.target.value)
+}
+
+export const getUser = (setUser) => {
+  const options = {
+    headers: { Authorization: env.TOKEN_GITHUB }
+  }
+  axios.get(uri + userDefault, options)
+    .then(response => setUser(response.data))
 }
